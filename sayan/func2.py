bf8d16b4878908b66b4d296d45e9b27d8b6c4c5b
annup76779 <annup76779@gmail.com>
2022-10-09 16:39:00 +0530

    09-10-2022

--- sayan/func2.py
+++ sayan/func2.py
@@ -1,33 +1,16 @@
-# i want to use both the function I have made namely addition() and multiply()
-## i want to use the result of the addition as num1 in the multiply()
+# " ".join(["anurag", "pandey"])
 
-def addition():
-	print("\n--------------------------------------")
-	num1 = int(input("Enter the first number: "))
-	num2 = int(input("Enter the second number: "))
-	return num1 + num2
+def concate_for_me(sq, *, sep = " ", is_str = True, ends_with = "") -> str:
+	assert isinstance(sq, (list, tuple))
+	# if assert is True, code works else AssertionError will be raised
+	assert isinstance(sep, str) # seperator must be string
+	if is_str:
+		res = sep.join(sq)
+		return res + ends_with
+	else:
+		res = sep.join([str(sq_Data) for sq_Data in sq])
+		return res + ends_with
 
+print(concate_for_me(["Anurag", "Pandey"], sep = " 5 ", ends_with = " is fine coder."))
 
-def multiply():
-	print("\n--------------------------------------")
-	num1 = addition() # we get the returned value from addition() saved in num1
-	num2 = int(input("Enter the number to multiply"))
-	print(num1 * num2)
-
-multiply()
-
-
-# what is hard coding any data
-# when you write directly any data into your code
-# instead of taking that data from any data source like
-# user input or database or file, that way of taking data
-# is called hardcoding of the data
-
-## what is the best practice to write code when working with function 
-## we have to always try to write function which only perform one functionality of the big program 
-
-
-##################
-# return statement
-##################
-# it is used in function to give the cooked result of the function back to the caller pointer
+# I have to teach you how to check if all the data in list is of specific type
